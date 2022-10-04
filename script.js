26a9664c25cea2dc01d66a7986bebe67e3143284
Kevin Karia <>
2022-10-04 11:31:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/*  */
+/* Burger Menu */
+/*  */
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active_bur')
+})
+
+/*  */
+/* FAQs */
+/*  */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
